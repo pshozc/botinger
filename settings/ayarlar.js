6b36100b0aa67a0eb5a144ef1473b50bf16e33a4
Paşahan Özcan <70802419+pshozc@users.noreply.github.com>
2023-07-16 23:15:23 +0300

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTEzMDA5NTI4Mzc1MDA0Nzc1NA.G0ZAeg.PaDbEPxEVSo8EBI4s6PfhpYB85TO2DIGANtr10",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "669903399998980116", //sahip id :()
+}
